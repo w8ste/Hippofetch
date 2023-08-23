be838533adbdd7c142bd37baca7f95cfb13eeae4
Hannes Albert <hannes10042004@icloud.com>
2023-08-24 01:18:18 +0200

    Cleaned up code

--- test.cpp
+++ test.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <chrono>
 #include <ctime>    
+#include <string>
 
-int main() {
+std::time_t getTime() {
     auto start = std::chrono::system_clock::now();
  
     std::time_t time = std::chrono::system_clock::to_time_t(start);
  
+    return time;
+    
+}
+int main() {
+    std::time_t time = getTime();
     std::cout << std::ctime(&time);
+    return 0;
 }
