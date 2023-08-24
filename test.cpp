7db88d3a7126ba38b62bd8a2db3157b526cf327a
Hannes Albert <hannes10042004@icloud.com>
2023-08-24 12:38:16 +0200

    Cleaned up

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 #include <chrono>
 #include <ctime>    
 #include <string>
-
+#include <stdio.h>
 std::time_t getTime() {
     auto start = std::chrono::system_clock::now();
  
@@ -11,8 +11,29 @@ std::time_t getTime() {
     return time;
     
 }
+
+std::string getOsName()
+{
+    #ifdef _WIN32
+    return "Windows 32-bit";
+    #elif _WIN64
+    return "Windows 64-bit";
+    #elif __APPLE__ || __MACH__
+    return "Mac OSX";
+    #elif __linux__
+    return "Linux";
+    #elif __FreeBSD__
+    return "FreeBSD";
+    #elif __unix || __unix__
+    return "Unix";
+    #else
+    return "Other";
+    #endif
+}
+
 int main() {
     std::time_t time = getTime();
     std::cout << std::ctime(&time);
+    std::cout << getOsName();
     return 0;
 }
